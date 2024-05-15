308045c78593a083928ecbac92b8063866526ddb
Edwille Báez <baezedwille@gmail.com>
2024-05-15 14:16:09 -0400

    adding loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,73 @@
-import HeroSection from "./components/HeroSection";
-import Navbar from "./components/Navbar";
+import { Suspense } from 'react';
+import { lazy } from 'react';
+
+const HeroSection = lazy(() => import("./components/HeroSection"));
+const Navbar = lazy(() => import("./components/Navbar"));
 import "./app.css";
-import Introduction from "./components/Introduction";
-import Separator from "./components/Separator";
-import ImageTextSection from "./components/ImageTextSection";
-import Projects from "./components/Projects";
-import Footer from "./components/Footer";
-import Phrase from "./components/Phrase";
-import BackGround from "./components/BackGround";
-// import Goals from "./components/Goals";
+import Loader from './components/Loader';
+import BackGround from './components/BackGround';
+const Introduction = lazy(() => import("./components/Introduction"));
+const Separator = lazy(() => import("./components/Separator"));
+const ImageTextSection = lazy(() => import("./components/ImageTextSection"));
+const Projects = lazy(() => import("./components/Projects"));
+const Footer = lazy(() => import("./components/Footer"));
+const Phrase = lazy(() => import("./components/Phrase"));
+// const Goals = lazy(() => import("./components/Goals"));
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <BackGround />
-      <HeroSection />
-      <Introduction />
-      <ImageTextSection
-        id="about-me"
-        imageUrl="/assets/image-2.jpg"
-        title="Algo sobre mí"
-        subtitle="Objetivo personal"
-        paragraphIndex={0}
-        bgColor="bg-white"
-        imagePosition="left"
-      />
-      <Separator backgroundImage="/assets/image-1.jpg" className="sm:hidden"/>
-      <ImageTextSection
-        imageUrl="/assets/image-3.jpg"
-        title="Algo mas..."
-        subtitle="Objetivo profesional"
-        paragraphIndex={1}
-        bgColor="bg-secondary"
-        imagePosition="right"
-      />
-      <Separator title="MIS PROYECTOS" paragraph="Competencias profesionales"/>
-      <Projects />
-      <ImageTextSection
-        id="more-about-me"
-        imageUrl="/assets/image-4.jpg"
-        title="Algo sobre mí"
-        subtitle="Filosofia personal"
-        paragraphIndex={2}
-        bgColor="bg-white"
-        imagePosition="left"
-      />
-      <Separator backgroundImage="/assets/image-3.jpg" className="sm:hidden"/>
-      <ImageTextSection
-        imageUrl="/assets/image-5.jpg"
-        title="Algo mas..."
-        subtitle="Filosofia profesional"
-        paragraphIndex={3}
-        bgColor="bg-secondary"
-        imagePosition="right"
-      />
-      {/* <Separator
-        title="MIS HABILIDADES"
-        paragraph="Competencias personales"
-      /> */}
-      {/* <Goals/> */}
-      <Phrase />
-      <Footer />
+      <Suspense fallback={<Loader />}>
+        <Navbar />
+        <BackGround />
+        <HeroSection />
+        <Introduction />
+        <ImageTextSection
+          id="about-me"
+          imageUrl="/assets/image-2.jpg"
+          title="Algo sobre mí"
+          subtitle="Objetivo personal"
+          paragraphIndex={0}
+          bgColor="bg-white"
+          imagePosition="left"
+        />
+        <Separator backgroundImage="/assets/image-1.jpg" className="sm:hidden"/>
+        <ImageTextSection
+          imageUrl="/assets/image-3.jpg"
+          title="Algo mas..."
+          subtitle="Objetivo profesional"
+          paragraphIndex={1}
+          bgColor="bg-secondary"
+          imagePosition="right"
+        />
+        <Separator title="MIS PROYECTOS" paragraph="Competencias profesionales"/>
+        <Projects />
+        <ImageTextSection
+          id="more-about-me"
+          imageUrl="/assets/image-4.jpg"
+          title="Algo sobre mí"
+          subtitle="Filosofia personal"
+          paragraphIndex={2}
+          bgColor="bg-white"
+          imagePosition="left"
+        />
+        <Separator backgroundImage="/assets/image-3.jpg" className="sm:hidden"/>
+        <ImageTextSection
+          imageUrl="/assets/image-5.jpg"
+          title="Algo mas..."
+          subtitle="Filosofia profesional"
+          paragraphIndex={3}
+          bgColor="bg-secondary"
+          imagePosition="right"
+        />
+        {/* <Separator
+          title="MIS HABILIDADES"
+          paragraph="Competencias personales"
+        /> */}
+        {/* <Goals/> */}
+        <Phrase />
+        <Footer />
+      </Suspense>
     </div>
   );
 }
---
+++ src/components/Loader.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+interface LoaderProps {
+	size?: string
+}
+
+const Loader: React.FC<LoaderProps> = ({ size = '2rem' }) => {
+	return (
+		<div className="relative">
+			{' '}
+			<style>
+				{`
+                .loader {
+                display: flex;
+                transition: all 0.4s;
+                }
+
+                .loader div {
+                margin-left: 0.8rem;
+                background-color: transparent;
+                box-shadow: 0px 2px 2px black;
+                border-radius: 3em;
+                height: calc(${size} / 2);
+                width: ${size};
+                }
+
+                .box-load1 {
+                animation: brighten 1.5s infinite;
+                }
+
+                .box-load2 {
+                animation: brighten 1.5s infinite;
+                animation-delay: .3s;
+                }
+
+                .box-load3 {
+                animation: brighten 1.5s infinite;
+                animation-delay: .6s;
+                }
+
+                @keyframes brighten {
+                100% {
+                        background-color: #FF5769;
+                    box-shadow: none;
+                }
+                }
+
+                `}
+			</style>
+			<div className="loader flex justify-center items-center w-screen h-screen">
+				<div className="box-load1"></div>
+				<div className="box-load2"></div>
+				<div className="box-load3"></div>
+			</div>
+		</div>
+	)
+}
+
+export default Loader
